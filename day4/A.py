4a5121631163dc2b89f8705787b7a8ad1c57b75a
CaraTortu <javierdj@jadk.com>
2022-12-04 12:30:52 +0000

    day 4

---
+++ day4/A.py
@@ -0,0 +1,23 @@
+def overlapping(A: list, B: list) -> bool:
+    for i in A:
+        if i not in B:
+            return False
+    return True
+
+overlap = 0
+
+for line in open("./day4/input").readlines():
+    line = line.strip()
+    
+    elfA, elfB = line.split(",")
+
+    elfAStart, elfAEnd = elfA.split("-")
+    elfBStart, elfBEnd = elfB.split("-")
+
+    elfARange = list(range(int(elfAStart), int(elfAEnd)+1))
+    elfBRange = list(range(int(elfBStart), int(elfBEnd)+1))
+
+    if overlapping(elfARange, elfBRange) or overlapping(elfBRange, elfARange):
+        overlap += 1
+
+print(overlap)
